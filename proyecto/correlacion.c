37364e1350b4f78f068ce3a0b064991d76bd31d4
Gutierrez Rivera Marco Alexis <gutierrezrm2018@dci25-192.ugto.mx>
2018-11-28 09:59:31 -0600

    creacion de la carpeta y el archivo inicial

---
+++ proyecto/correlacion.c
@@ -0,0 +1,11 @@
+/*Proyecto final de Programación Básica (funcion_correlacion).
+Programa hecho por:
+-Gutiérrez Rivera Marco Alexis
+-García Ramírez Rafael Alejandro
+el 28/noviembre/2018*/
+
+#include<stdio.h>
+
+int main()
+{
+	
