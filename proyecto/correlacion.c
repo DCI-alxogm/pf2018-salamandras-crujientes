1f9e4e4f822feb42b5c7f7dd6c43f31b73d20315
Gutierrez Rivera Marco Alexis <marckcartoon@gmail.com>
2018-12-03 09:11:03 -0600

    estructura base del programa

--- proyecto/correlacion.c
+++ proyecto/correlacion.c
@@ -8,4 +8,65 @@ el 28/noviembre/2018*/
 
 int main()
 {
+//declaracion de variables
+	FILE* fp;
+	int opcion,i;
+	float E[];
+	float x[3], y[3], Z[3];
+
+//escaneo de datos
+	fp=fopen("Datos.txt","r");
+	for(i=0;i<=3;i++)
+	{
+	fscanf("%f %f %f",x[i],y[i],z[i]);
+	}
+	fclose(fp);
+
+//calcular los valores de r
+	for(i=0;i<=3;i++)
+	{
+		for(j=i+1;j<=3;j++)
+		{
+		r[k]=sqrt(pow((x[i]-x[j]),2)+pow((y[i]-y[j]),2)+pow((z[i]-z[j]),2))
+		}
+	}
+
+
+
+
+
+
+
+//valores de prueba
+	nD=3;
+	nR=3;
 	
+
+
+//el usuario escoge que estimador usar
+	printf("Escoja el estimador que quiera usar:\n");
+	printf("1: Estimador simple\n2: Estimador Landy-szalay\n");
+	scanf("%i",&opcion);
+
+	switch(opcion)
+	{
+	case 1:	
+		E[r]=((nR/nD)*(DD[r]/RR[r]))-1;
+	
+
+
+
+		break;
+	case 2:	
+		E[r]=((DD[r]/nD)-(2*DR[r]/nDR)-(RR[r]/nR))/(RR[r]/nR);
+
+
+
+
+
+		break;
+	default:
+		printf("No escojio una opcion valida\n");
+	}
+return 0;
+}
