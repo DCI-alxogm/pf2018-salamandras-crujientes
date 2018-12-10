cc953d47d03369a7a7782b5b54da5753e8f2c257
MarcoGtz <marckcartoon@gmail.com>
2018-12-09 19:00:31 -0600

    terminado

--- proyecto/correlacion.c
+++ proyecto/correlacion.c
@@ -7,18 +7,17 @@ el 28/noviembre/2018*/
 #include<stdio.h>
 #include<math.h>
 
-int main()
+int main(int bin, int opcion, int L, char nombre[40])
 {
 //***********************************
 //	declaracion de variables
 //***********************************
 	FILE* fp;
-	int i, j, k, opcion;
-	int num_lineas;
+	int i, j, k;
+	int num_lineas1, num_lineas2;
 	int nD, nR, nDR;
-	int DD[3], RR[3];  //tamaño de los arreglos solo de prueba
-	float E[];
-	float x[3], y[3], z[3], rD[3], rR[3];  //tamaño de los arreglos solo de prueba
+	int DD[L/bin], RR[L/bin], DR[L/bin];
+	float E;
 	char ch;
 
 
@@ -26,23 +25,27 @@ int main()
 //**********************************************
 //	contador de renglones de Datos.txt
 //**********************************************
-	num_lineas=0;
+	num_lineas1=0;
 	fp=fopen("Datos.txt","r");
 	while((ch=fgetc(fp))!=EOF)
 	{
 		if(ch=='\n')
 		{
-		num_lineas++;
+		num_lineas1++;
 		}
 	}
 	fclose(fp);
+	float x1[num_lineas1], y1[num_lineas1], z1[num_lineas1];
 
-	for(i=num_lineas-1;i>0;i--)
+	//calculamos nD
+	for(i=num_lineas1-1;i>0;i--)
 	{
 	nD=nD+i;
 	}
-
-	nDR=num_lineas;
+	//inicializamos rD siendo mayor que la r maxima
+	float rD[nD];
+	for(i=0;i<nD;i++){
+	rD[i]=sqrt(3*L*L)+1;}
 
 
 
@@ -50,9 +53,9 @@ int main()
 //	escaneo de Datos.txt
 //*******************************
 	fp=fopen("Datos.txt","r");
-	for(i=0;i<num_lineas;i++)
+	for(i=0;i<num_lineas1;i++)
 	{
-	fscanf(fp,"%f %f %f",&x[i],&y[i],&z[i]);
+	fscanf(fp,"%f %f %f",&x1[i],&y1[i],&z1[i]);
 	}
 	fclose(fp);
 
@@ -62,42 +65,47 @@ int main()
 //	calcular los valores de r de Datos.txt
 //*************************************************
 	k=0;
-	for(i=0;i<num_lineas;i++)
+	for(i=0;i<num_lineas1;i++)
 	{
-		for(j=i+1;j<num_lineas;j++)
+		if(x1[i]<=L && y1[i]<=L && z1[i]<=L)
 		{
-		rD[k]=sqrt(pow((x[i]-x[j]),2)+pow((y[i]-y[j]),2)+pow((z[i]-z[j]),2));
-		k++;
+			for(j=i+1;j<num_lineas1;j++)
+			{
+				if(x1[j]<=L && y1[j]<=L && z1[j]<=L)
+				{
+				rD[k]=sqrt(pow((x1[i]-x1[j]),2)+pow((y1[i]-y1[j]),2)+pow((z1[i]-z1[j]),2));
+				k++;
+				}
+			}
 		}
 	}
 
 
 
-//**********************************************
+//**************************************************
 //	contador de renglones de Aleatorios.txt
-//**********************************************
-	num_lineas=0;
+//**************************************************
+	num_lineas2=0;
 	fp=fopen("Aleatorios.txt","r");
 	while((ch=fgetc(fp))!=EOF)
 	{
 		if(ch=='\n')
 		{
-		num_lineas++;
+		num_lineas2++;
 		}
 	}
 	fclose(fp);
+	float x2[num_lineas2], y2[num_lineas2], z2[num_lineas2];
 
-	for(i=num_lineas-1;i>0;i--)
+	//calculamos nR
+	for(i=num_lineas2-1;i>0;i--)
 	{
 	nR=nR+i;
 	}
-
-	num_lineas+=nDR;
-	nDR=0;
-	for(i=num_lineas-1;i>0;i--)
-	{
-	nDR+=i;
-	}
+	//inicializamos rR siendo mayor que la r maxima
+	float rR[nR];
+	for(i=0;i<nR;i++){
+	rR[i]=sqrt(3*L*L)+1;}
 
 
 
@@ -105,9 +113,9 @@ int main()
 //	escaneo de Aleatorios.txt
 //***********************************
 	fp=fopen("Aleatorios.txt","r");
-	for(i=0;i<num_lineas;i++)
+	for(i=0;i<num_lineas2;i++)
 	{
-	fscanf(fp,"%f %f %f",&x[i],&y[i],&z[i]);
+	fscanf(fp,"%f %f %f",&x2[i],&y2[i],&z2[i]);
 	}
 	fclose(fp);
 
@@ -117,12 +125,71 @@ int main()
 //	calcular los valores de r de Aleatorios.txt
 //*******************************************************
 	k=0;
-	for(i=0;i<num_lineas;i++)
+	for(i=0;i<num_lineas2;i++)
 	{
-		for(j=i+1;j<num_lineas;j++)
+		if(x2[i]<=L && y2[i]<=L && z2[i]<=L)
 		{
-		rR[k]=sqrt(pow((x[i]-x[j]),2)+pow((y[i]-y[j]),2)+pow((z[i]-z[j]),2));
-		k++;
+			for(j=i+1;j<num_lineas2;j++)
+			{
+				if(x2[j]<=L && y2[j]<=L && z2[j]<=L)
+				{
+				rR[k]=sqrt(pow((x2[i]-x2[j]),2)+pow((y2[i]-y2[j]),2)+pow((z2[i]-z2[j]),2));
+				k++;
+				}
+			}
+		}
+	}
+
+
+
+//**************************************
+//	obtenemos el valor de nDR
+//**************************************
+	nDR=0;
+	for(i=num_lineas1+num_lineas2;i>0;i--)
+	{
+	nDR+=i;
+	}
+	//inicializamos rDR siendo mayor que la r maxima
+	float rDR[nDR];
+	for(i=0;i<nDR;i++){
+	rDR[i]=sqrt(3*L*L)+1;}
+
+	//creamos un arreglo con las coordenadas para DR
+	float x[num_lineas1+num_lineas2], y[num_lineas1+num_lineas2], z[num_lineas1+num_lineas2];
+
+	for(i=0;i<num_lineas1;i++)
+	{
+	x[i]=x1[i];
+	y[i]=y1[i];
+	z[i]=z1[i];
+	}
+
+	for(i=num_lineas1;i<num_lineas2;i++)
+	{
+	x[i]=x2[i-num_lineas1];
+	y[i]=y2[i-num_lineas1];
+	z[i]=z2[i-num_lineas1];
+	}
+
+
+
+//*****************************************
+//	calculamos los valores de rDR
+//*****************************************
+	k=0;
+	for(i=0;i<num_lineas1+num_lineas2;i++)
+	{
+		if(x[i]<=L && y[i]<=L && z[i]<=L)
+		{
+			for(j=i+1;j<num_lineas1+num_lineas2;j++)
+			{
+				if(x[j]<=L && y[j]<=L && z[j]<=L)
+				{
+				rDR[k]=sqrt(pow((x[i]-x[j]),2)+pow((y[i]-y[j]),2)+pow((z[i]-z[j]),2));
+				k++;
+				}
+			}
 		}
 	}
 
@@ -164,33 +231,109 @@ int main()
 
 
 
+//***********************************************************
+//	ordenamos los valores de rDR del menor al mayor
+//***********************************************************
+	for(i=0;i<nDR;i++)
+	{
+		for(j=i+1;j<nDR;j++)
+		{
+			if(rDR[j]<rDR[i])
+			{
+			k=rDR[i];
+			rDR[i]=rDR[j];
+			rDR[j]=k;
+			}
+		}
+	}
+
+
+
+//******************************************
+//	inicializamos DD, RR y DR en ceros
+//******************************************
+	for(i=0;i<L/bin;i++){
+	DD[i]=0;}
+	for(i=0;i<L/bin;i++){
+	RR[i]=0;}
+	for(i=0;i<L/bin;i++){
+	DR[i]=0;}
+
+
+
+//*******************************************************
+//	valores del histograma DD con tamaño de bin
+//*******************************************************
+	for(j=bin;j<L/bin;j+=bin)
+	{
+		for(i=0;i<nD;i++)
+		{
+			if(rD[i]<=j)
+			{
+			DD[(j/bin)-1]++;
+			}
+		}
+	}
+
+
+
+//*******************************************************
+//	valores del histograma RR con tamaño de bin
+//*******************************************************
+	for(j=bin;j<L/bin;j+=bin)
+	{
+		for(i=0;i<nR;i++)
+		{
+			if(rR[i]<=j)
+			{
+			RR[(j/bin)-1]++;
+			}
+		}
+	}
+
+
+
+//*******************************************************
+//	valores del histograma DR con tamaño de bin
+//*******************************************************
+	for(j=bin;j<L/bin;j+=bin)
+	{
+		for(i=0;i<nDR;i++)
+		{
+			if(rDR[i]<=j)
+			{
+			DR[(j/bin)-1]++;
+			}
+		}
+	}
+
+
+
 //************************************************
 //	el usuario escoge que estimador usar
 //************************************************
-	printf("\nEscoja el estimador que quiera usar:\n");
-	printf("1: Estimador simple\n2: Estimador Landy-szalay\n");
-	scanf("%i",&opcion);
-
 	switch(opcion)
 	{
-	case 1:	
-		fp=fopen("Resultados_Estimador.txt","w")
-		for(i=0;i;i)
+	case 1:	//estimador simple
+		fp=fopen("Resultados_Estimador.txt","w");
+		for(i=0;i<L/bin;i++)
 		{
-		fprintf(fp,"%f",r[i]);//cambiar
-		E[i]=((nR/nD)*(DD[r]/RR[r]))-1;
+		fprintf(fp,"%i-%i \t",bin*i,bin*(i+1));
+		E=((nR/nD)*(DD[i]/RR[i]))-1;
+		fprintf(fp,"%f\n",E);
 		}
-
-
 		fclose(fp);
 		break;
-	case 2:	
-		E[r]=((DD[r]/nD)-(2*DR[r]/nDR)+(RR[r]/nR))/(RR[r]/nR);
 
-
-
-
-
+	case 2:	//estimador de landy-szalay
+		fp=fopen("Resultados_Estimador.txt","w");
+		for(i=0;i<L/bin;i++)
+		{
+		fprintf(fp,"%i-%i \t",bin*i,bin*(i+1));
+		E=((DD[i]/nD)-(2*DR[i]/nDR)+(RR[i]/nR))/(RR[i]/nR);
+		fprintf(fp,"%f\n",E);
+		}
+		fclose(fp);
 		break;
 	default:
 		printf("\nNo seleccionó una opción válida\n");
